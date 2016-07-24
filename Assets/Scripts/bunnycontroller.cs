e7d56b880cefc541c57dfbc578325baa02b9e08f
alexbuckley1995 <Alex-Buckley@live.nmit.ac.nz>
2016-07-24 18:24:55 +1200

    Added bunny animations.

--- Assets/Scripts/bunnycontroller.cs
+++ Assets/Scripts/bunnycontroller.cs
@@ -4,11 +4,13 @@ using System.Collections;
 public class bunnycontroller : MonoBehaviour {
 
     private Rigidbody2D myrigidbody;
+    private Animator myAnim;
     public float bunnyjumpforce = 500f;
 
 	// Use this for initialization
 	void Start () {
         myrigidbody = GetComponent<Rigidbody2D>();
+        myAnim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -17,5 +19,6 @@ public class bunnycontroller : MonoBehaviour {
         {
             myrigidbody.AddForce(transform.up * bunnyjumpforce);
         }
+        myAnim.SetFloat("vVelocity", myrigidbody.velocity.y); //SetFloat method lets us set any float parameter we have set on the animator. I am calling the parameter vVelocity, and we tie it to the rigidbody's velocity in the y direction. The rigidbody is  controlling the physics. What this does in every frame update we are going to on the animator set its vVelocity parameter to the current velocity of the bunny as he is either falling down or jumping up. 
 	}
 }
