ee4a8a2302cbfa18a22c7e2e93d1c208c8fb91c9
alexbuckley1995 <Alex-Buckley@live.nmit.ac.nz>
2016-07-26 20:33:46 +1200

    Tweaked gameplay

--- Assets/PrefabSpawner.cs
+++ Assets/PrefabSpawner.cs
@@ -5,12 +5,15 @@ public class PrefabSpawner : MonoBehaviour {
 
     private float nextSpawn = 0; //this is a private variable internal to the class. This will hold the next point in time we want to spawn a cactus. 
     public Transform prefabToSpawn; //This will hold a reference to the prefab we want to spawn in this case the cactus prefab 
-    public float spawnRate = 1; //Time in seconds between spawning our prefabs
-    public float randomDelay = 1; //This will make cactus not spawn exactly at the spawn rate but instead the spawn rate plus some random delay
+    public AnimationCurve spawnCurve;
+    public float curveLengthInSeconds = 30f; //This is mapping the curve time length to 30 seconds (i.e. it takes 30 seconds for the curve to be completed from spawning a cactus one every second at the start to a cactus all the time at the end) 
+    private float startTime;
+
+    public float jitter = 0.25f; 
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start () { //This gets called when the prefabSpawner gets created
+        startTime = Time.time; //Set the startTime to the current game time this lets us know how long it has been since the prefabSpawner has been created. 
 	}
 	
 	// Update is called once per frame
@@ -18,8 +21,19 @@ public class PrefabSpawner : MonoBehaviour {
 	
         if(Time.time > nextSpawn ) //Time.time is the current game time is past our next spawn time, then spawn
         {
-            Instantiate(prefabToSpawn, transform.position, Quaternion.identity); //passing into the instantiation method the prefab method I want to spawn (prefabToSpawn), current position (which is transform which is the game object this script is attached to which is our cactus spawn point game object) , and the rotation (because we don't want to rotate it so we write Quaternion.identity which is a zero value)
-            nextSpawn = Time.time + spawnRate + Random.Range(0, randomDelay); //This takes the current game time adds the spawnRate to it (which is by default 1 second) and then add an additional random amount of time between 0 and randomDelay
+            Instantiate(prefabToSpawn, transform.position, Quaternion.identity); //passing into the instantiation method the prefab method I want to spawn (prefabToSpawn), current position (which is transform which is the game object this script is attached to which is our cactus spawn point game object) , and the rotation (because we don't want to rotate it so we write Quaternion.identity which is a zero value) so all together this instatiates the cactus 
+                                                                                 //nextSpawn = Time.time + spawnRate + Random.Range(0, randomDelay); //This takes the current game time adds the spawnRate to it (which is by default 1 second) and then add an additional random amount of time between 0 and randomDelay
+
+            float curvePos = (Time.time - startTime) / curveLengthInSeconds; //Calculate our position on a curve (between 0 and 1) by taking the current time and subtracting the startTime this tells us the fractional number of seconds that have passed since the prefab spanner was created and divide this by curveLengthInSeconds (curveLengthInSeconds =30) so this results in the calculation being the number of seconds that have passed divided by 30 and this gives us a value between 0 and 1 which is the position along the curve. 
+
+            if (curvePos > 1f) //Because the curve is between 0 and 1 once we get past 30 seconds we will get a value greater than 1 so we want to cap that so this If statement is checking if we have gone past 1. The contents of this If statement will cause the game to go through the curve getting increasingly faster and then slow down again before getting faster and faster again. 
+            {
+                curvePos = 1f; //Set the curvePos =1.
+                startTime = Time.time; //Reset the starttime to current time. 
+            }
+
+            nextSpawn = Time.time + spawnCurve.Evaluate(curvePos) + Random.Range(-jitter, jitter); //Now we have our position on the curve we need to calculate our next spawn time, this line makes the nextSpawn equal to the current time + spawnCurve (which is our animation curve we defined at the top) spawnCurve has a method on it called Evaluate (to which we pass in a value (which is the position on the curve, the horizontal axis of the graph ) we pass in curvePos which is the value we have calculcated which will be some value between 0 and 1. We add to this a random number (from -jitter(-0.25) to jitter +(0.25)) this will allow us to shrink or grow the spawn time by a random amount. 
+
         }
 
 
--- Assets/Scripts/bunnycontroller.cs
+++ Assets/Scripts/bunnycontroller.cs
@@ -11,6 +11,7 @@ public class bunnycontroller : MonoBehaviour {
     private Collider2D myCollider;
     public Text scoreText;
     private float startTime;
+    private int jumpsLeft = 2; //The number of jumps we have left which is set to 2 initially 
 
 	// Use this for initialization
 	void Start () {
@@ -28,9 +29,24 @@ public class bunnycontroller : MonoBehaviour {
         if (bunnyHurtTime == -1) //When the game starts and whilst it is running it is going to do the button checking and animation setting (as when the game starts the bunnyHurtTime is set to -1), up until we actually collide with something (when we set bunnyHurtTime to a non -1 value). This if statement is to ensure that if the bunny is dead (i.e. bunnyHurtTime is not -1) then it cannot jump as we dont want that.
         {
 
-            if (Input.GetButtonUp("Jump"))
+            if (Input.GetButtonUp("Jump") && jumpsLeft >0) //If the jump button (space button) has been pressed and jumpsLeft>0 (in other words we have jumpsLeft) then the bunny is allowed to jump by adding force below. 
             {
-                myrigidbody.AddForce(transform.up * bunnyjumpforce);
+
+                if (myrigidbody.velocity.y <0) //Check if the bunnys velocity is <0 (i.e. falling).
+                {
+                    myrigidbody.velocity = Vector2.zero; //If bunny is falling then cancel out any velocity. 
+                }
+
+                if (jumpsLeft == 1) //Check if this is the second jump
+                {
+                    myrigidbody.AddForce(transform.up * bunnyjumpforce * 0.75f); //Bunny jump but make it only 3/4 as powerful as the first jump (i.e. reduced to 75%)
+                }
+                else //If the bunnyJump is not equal to 1 
+                {
+                    myrigidbody.AddForce(transform.up * bunnyjumpforce ); //full force Bunny jump 
+                }
+                jumpsLeft--; //Reduce the jumpsLeft value by 1
+
             }
             myAnim.SetFloat("vVelocity", myrigidbody.velocity.y); //SetFloat method lets us set any float parameter we have set on the animator. I am calling the parameter vVelocity, and we tie it to the rigidbody's velocity in the y direction. The rigidbody is  controlling the physics. What this does in every frame update we are going to on the animator set its vVelocity parameter to the current velocity of the bunny as he is either falling down or jumping up. 
 
@@ -61,10 +77,14 @@ public class bunnycontroller : MonoBehaviour {
 
             bunnyHurtTime = Time.time; //Set the bunnyHurtTime equal to the current time 
             myAnim.SetBool("bunnyHurt", true); //Set the bunnyHurt animation to true this will trigger the bunnyHurt animation.
-            myrigidbody.velocity = Vector2.zero; //RigidBody controls the physics for the bunny this cancels out any motion it has. So when the bunny coliides set its velocity movement to zero and 
+            myrigidbody.velocity = Vector2.zero; //RigidBody controls the physics for the bunny this cancels out any motion it has. So when the bunny coliides set its velocity movement to zero.
             myrigidbody.AddForce(transform.up * bunnyjumpforce); //Trigger it to jump up in the air
             myCollider.enabled = false; //Now we have the myCollider object in the start method and disable the collider 
+        }
 
+        else if (collision.collider.gameObject.layer ==LayerMask.NameToLayer("Ground")) //else if the object we've collided with (collision.collider.gameObject.layer) is equal to ground) then reset our jumpsLeft. 
+        {
+            jumpsLeft = 2; //When we hit the ground we set the number of jumps back to 2
         }
     }
 }
